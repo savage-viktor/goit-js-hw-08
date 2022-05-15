921029af72dd6ea4568d9451322ec26db4447e84
savage-viktor <viktor.dkn@gmail.com>
2022-05-15 23:14:55 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('timeupdate', function ({ seconds }) {
+    localStorage.setItem("videoplayer-current-time", seconds)
+});
+
+let currentTime = 0;
+
+if (localStorage.getItem("videoplayer-current-time")) {
+    currentTime = localStorage.getItem("videoplayer-current-time");
+}
+
+player.setCurrentTime(currentTime)
