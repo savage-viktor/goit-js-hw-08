8536afd9106a8e3123fa8b85d112e670d170d50c
savage-viktor <viktor.dkn@gmail.com>
2022-05-15 19:33:29 +0300

    Task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,14 +2,15 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const basicLightbox = require("basiclightbox")
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const galleryDivRef = document.querySelector(".gallery")
 
 galleryDivRef.innerHTML = galleryItems.map(({ preview, original, description }) => {
 
-  return `<div class="gallery__item" href="${original}" >
-        <a class="gallery__link" >
+  return `<div class="gallery__item"  >
+        <a class="gallery__link" href="${original}">
             <img
                 class="gallery__image"
                 src="${preview}"
@@ -20,18 +21,8 @@ galleryDivRef.innerHTML = galleryItems.map(({ preview, original, description }) 
 </div >`
 }).join("")
 
-function openModalBigImg(event) {
-  event.preventDefault();
-  if (event.target === galleryDivRef) {
-    return
-  }
 
-  const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
-
-  instance.show()
-
-}
-
-galleryDivRef.addEventListener("click", openModalBigImg)
+new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionDelay: 250,
+});
