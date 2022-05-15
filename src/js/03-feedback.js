7a97632bf82cb2058ef93d8c1e66b579ea5bfe73
savage-viktor <viktor.dkn@gmail.com>
2022-05-16 00:42:11 +0300

    Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+const formRef = document.querySelector(".feedback-form")
 
+const toSave = { email: "", message: "" };
+
+if (localStorage.getItem('feedback-form-state')) {
+
+    const objectFromStorage = JSON.parse(localStorage.getItem('feedback-form-state'))
+
+    toSave.email = objectFromStorage.email
+    toSave.message = objectFromStorage.message
+}
+
+formRef.email.value = toSave.email
+formRef.message.value = toSave.message
+
+function inputEmail(eve) {
+    if (eve.target.name === "email") {
+        toSave.email = eve.target.value
+    }
+    if (eve.target.name === "message") {
+        toSave.message = eve.target.value
+    }
+    localStorage.setItem('feedback-form-state', JSON.stringify(toSave))
+}
+
+function submitFn(eve) {
+    eve.preventDefault()
+    eve.currentTarget.reset();
+    localStorage.clear()
+    console.log(toSave)
+}
+
+formRef.addEventListener("input", inputEmail)
+formRef.addEventListener("submit", submitFn)
