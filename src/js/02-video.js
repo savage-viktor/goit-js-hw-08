73650ca042f9f1d952a967e13820c10b34e0edc9
savage-viktor <viktor.dkn@gmail.com>
2022-05-16 11:17:02 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,10 +2,9 @@ import throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', function ({ seconds }) {
-    throttle(localStorage.setItem("videoplayer-current-time", seconds), 1000)
-    // localStorage.setItem("videoplayer-current-time", seconds)
-});
+player.on('timeupdate', throttle(function ({ seconds }) {
+    localStorage.setItem("videoplayer-current-time", seconds)
+}, 1000));
 
 let currentTime = 0;
 
